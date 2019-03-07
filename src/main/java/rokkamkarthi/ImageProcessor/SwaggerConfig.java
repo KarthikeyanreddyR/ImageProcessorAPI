81227e341711f289296dc10b3e3344d21af76f1c
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-03-06 21:25:25 -0800

    enabled CORS and manually configured Swagger

--- src/main/java/rokkamkarthi/ImageProcessor/SwaggerConfig.java
+++ src/main/java/rokkamkarthi/ImageProcessor/SwaggerConfig.java
@@ -4,8 +4,10 @@
 package rokkamkarthi.ImageProcessor;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import rokkamkarthi.ImageProcessor.controller.ImageProcessorController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -18,13 +20,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@ComponentScan(basePackageClasses = {ImageProcessorController.class})
 public class SwaggerConfig {
 	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
-    }
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
 }
--- src/main/java/rokkamkarthi/ImageProcessor/WebConfig.java
+++ src/main/java/rokkamkarthi/ImageProcessor/WebConfig.java
@@ -3,6 +3,7 @@ package rokkamkarthi.ImageProcessor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,13 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowCredentials(true);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		 // Make Swagger meta-data available via <baseURL>/v2/api-docs/
+	    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	    // Make Swagger UI available via <baseURL>/swagger-ui.html
+	    registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/");
 	}
 }
