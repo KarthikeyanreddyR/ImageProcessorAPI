f2bc5cf1862fa6535cd15760b52c30ffa8b08dbd
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-03-12 21:55:57 -0700

    added case-insensitive support

--- src/main/java/rokkamkarthi/ImageProcessor/models/Orientation.java
+++ src/main/java/rokkamkarthi/ImageProcessor/models/Orientation.java
@@ -1,5 +1,12 @@
 package rokkamkarthi.ImageProcessor.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Orientation {
 	HORIZONTAL, VERTICAL, LEFT, RIGHT, UNKNOWN;
+	
+	@JsonCreator // This is the factory method and must be static
+    public static Orientation fromString(String orientation) {
+		return Orientation.valueOf(orientation.toUpperCase());
+    }
 }
--- src/main/java/rokkamkarthi/ImageProcessor/models/TransformType.java
+++ src/main/java/rokkamkarthi/ImageProcessor/models/TransformType.java
@@ -1,7 +1,14 @@
 package rokkamkarthi.ImageProcessor.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum TransformType {
 
 	GRAYSCALE, FLIP, ROTATE, RESIZE, THUMBNAIL, UNKNOWN;
+	
+	@JsonCreator // This is the factory method and must be static
+    public static TransformType fromString(String transformType) {
+		return TransformType.valueOf(transformType.toUpperCase());
+    }
 
 }
